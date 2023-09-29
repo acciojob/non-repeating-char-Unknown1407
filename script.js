d328ec5b504be43fb5194b7379ebb969c700ec14
Biswadeep Dutta <118428191+Unknown1407@users.noreply.github.com>
2023-09-29 12:17:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const count = {}
+
+	for(let char of str)
+		{
+			count[char] = (count[char] || 0)+1;
+		}
+
+	for(let char of str)
+		{
+			if(count[char] === 1)
+			{
+				return char;
+			}
+			else
+			{
+				return ("null");
+			}
+		}
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
